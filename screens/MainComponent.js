f78bc73052bbd70a7fc08321cfb2612d9150b3b3
antoniocorralg <gacorral@protonmail.com>
2022-06-18 12:34:59 -0700

    week 1 workshop completed

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -2,6 +2,8 @@ import { Platform, View } from "react-native";
 import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
@@ -13,10 +15,15 @@ const screenOptions = {
   headerStyle: { backgroundColor: "#5637DD" },
 };
 
+const altBackgroundColor = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#512C62" },
+};
+
 const HomeNavigator = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator screenOptions={screenOptions}>
+    <Stack.Navigator screenOptions={altBackgroundColor}>
       <Stack.Screen
         name="Home"
         component={HomeScreen}
@@ -46,6 +53,33 @@ const DirectoryNavigator = () => {
   );
 };
 
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={altBackgroundColor}>
+      <Stack.Screen
+        name="About"
+        component={AboutScreen}
+        // options={{ title: "Home" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Contact" screenOptions={altBackgroundColor}>
+      <Stack.Screen
+        name="Contact"
+        component={ContactScreen}
+        options={{ title: "Contact Us" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+
 const Main = () => {
   return (
     <View
@@ -67,6 +101,16 @@ const Main = () => {
           name="Directory"
           component={DirectoryNavigator}
           options={{ title: "Directory" }}
+        />
+        <Drawer.Screen
+          name="About"
+          component={AboutNavigator}
+          // options={{ title: "Directory" }}
+        />
+         <Drawer.Screen
+          name="Contact"
+          component={ContactNavigator}
+          options={{ title: "Contact Us" }}
         />
       </Drawer.Navigator>
     </View>
