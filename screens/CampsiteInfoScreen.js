b747f12decb548f617196b35d41a3ea7a444250e
antoniocorralg <gacorral@protonmail.com>
2022-09-06 21:10:28 -0700

    first react native app

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,57 @@
-import RenderCampsite from '../features/campsites/RenderCampsite';
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Render from "react-native-web/dist/cjs/exports/render";
+import RenderCampsite from "../features/campsites/RenderCampsite";
+import { COMMENTS } from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
-    const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />
-}
+  const { campsite } = route.params;
 
+  const renderCommentItem = ({ item }) => {
+    return(
+    <View style={styles.commentItem}>
+      <Text style={{ fontSize: 14 }}>{item.text}</Text>
+      <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+      <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
+    </View>
+  )};
+
+  const [comments, setComments] = useState(COMMENTS);
+  const [favorite, setFavorites] = useState(false);
+  return (
+    <FlatList
+      data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+      renderItem={renderCommentItem}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
+      ListHeaderComponent={
+        <>
+          <RenderCampsite
+            campsite={campsite}
+            isFavorite={favorite}
+            markFavorite={() => setFavorites(true)}
+          />
+          <Text style={styles.commentsTitle}>Comments</Text>
+        </>
+      }
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#43484D",
+    padding: 10,
+    paddingTop: 30,
+  },
+  commentItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+});
 export default CampsiteInfoScreen;
